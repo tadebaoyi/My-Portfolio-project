74949589d851463521081d4f311f6a2073f01e35
142500987 <142500987@mycct.cct.edu.za>
2026-04-08 12:53:46 +0200

    go

---
+++ Visual code/JavaScript/myScript.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Demo External JavaScript</h1>
+
+<p> A Paragraph</p>
+
+
+<button type="button" onclick="myFunction()">Change Content</button>
+
+<button type="button" onclick="myFunction()">Change Font Colour</button>
+
+<button type="button" onclick="myFunction()">Change Font Type</button>
+
+<p>This example links to "myScript.js".</p>
+<p>(myFunction is stored in "myScript.js")</p>
+
+<script src="myScript.js"></script>
+
+</body>
+</html>
